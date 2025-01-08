8c725f8b81b6888ff5b36703bdc201f50c8b3830
kravetsiv <kravetsiv@modeltech.ru>
2025-01-08 22:36:27 +0500

    "Сырой" бэк

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,7 @@
+import asyncio
+import re
+from functools import reduce
+
 import httpx
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
@@ -5,6 +9,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 SOURCE_HOST_URL = r"https://rus.hitmotop.com"
+REGEXP_LAST_START_FROM = r"\/search\/start\/(?P<last_start_from>\d+)\?q=(?P<q>.+)"
+LINK_PATTERN = "/search/start/{last_start_from}?q={q}"
+PER_PAGE_SIZE = 48
 
 
 def get_track_list(soup: BeautifulSoup):
@@ -32,6 +39,14 @@ def get_track_list(soup: BeautifulSoup):
     return tracks
 
 
+def generate_links_by_last_page(last_start_from: int, q: str):
+    links = [
+        LINK_PATTERN.format(last_start_from=r, q=q)
+        for r in range(PER_PAGE_SIZE, last_start_from + PER_PAGE_SIZE, PER_PAGE_SIZE)
+    ]
+    return links
+
+
 def get_pagination_links(soup: BeautifulSoup):
     links = []
 
@@ -39,11 +54,29 @@ def get_pagination_links(soup: BeautifulSoup):
     if pagination_list_tag is None:
         return links
 
+    pagination_last_tag = pagination_list_tag.find(
+        "a", class_="pagination__link", string=">>"
+    )
+    if pagination_last_tag:
+        href = pagination_last_tag["href"]
+        s = re.search(REGEXP_LAST_START_FROM, href)
+        if s is None:
+            return links
+
+        links.extend(
+            generate_links_by_last_page(int(s.group("last_start_from")), s.group("q"))
+        )
+    else:
+        link_tags = pagination_list_tag.find_all("a", class_="pagination__link")
+        links.extend([tag["href"] for tag in link_tags])
+
+    return links
+
 
 def parse_result(text: str):
     soup = BeautifulSoup(text, features="html.parser")
 
-    tracks = get_tracks(soup)
+    tracks = get_track_list(soup)
     links = get_pagination_links(soup)
 
     return tracks, links
@@ -56,8 +89,21 @@ async def get_web(url: str):
     return response.text
 
 
+async def get_results(url: str):
+    html = await get_web(url)
+    tracks, links = parse_result(html)
+
+    return tracks, links
+
+
 @app.get("/api/get-tracks")
 async def get_tracks(q: str):
-    html = await get_web(f"{SOURCE_HOST_URL}/search?q={q}")
+    tracks, links = await get_results(f"{SOURCE_HOST_URL}/search?q={q}")
 
-    tracks, links = parse_result(html)
+    tasks = [
+        asyncio.create_task(get_results(f"{SOURCE_HOST_URL}{link}")) for link in links
+    ]
+    results = await asyncio.gather(*tasks)
+    tracks.extend(reduce(lambda sum, val: sum + val[0], results, []))
+
+    return tracks
